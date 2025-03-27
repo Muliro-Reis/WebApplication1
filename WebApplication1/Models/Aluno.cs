c9f2b10f22d0fd18777a0f9db8b4644c9dba5e31
murilo.schiavotto@gmail.com <murilo.schiavotto@gmail.com>
2025-03-27 20:39:44 -0300

    Add project files.

---
+++ WebApplication1/Models/Aluno.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace WebApplication1.Models
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string RA { get; set; }
+
+        public static void GerarLista(HttpSessionStateBase session)
+        {
+            if (session["ListaAluno"] != null)
+            {
+                if (((List<Aluno>)session["ListaAluno"]).Count > 0)
+                {
+                    return;
+                }
+            }
+
+            var lista = new List<Aluno>();
+            lista.Add(new Aluno { Nome = "Allan Beroula", RA = "123" });
+            lista.Add(new Aluno { Nome = "Giuseppe Cadura", RA = "529" });
+            lista.Add(new Aluno { Nome = "Sujiro Kimimami", RA = "678" });
+
+            session.Remove("ListaAluno");
+            session.Add("ListaAluno", lista);
+        }
+
+        public void Adicionar(HttpSessionStateBase session)
+        {
+            if (session["ListaAluno"] != null)
+            {
+                (session["ListaAluno"] as List<Aluno>).Add(this);
+            }
+        }
+
+        public static Aluno Procurar(HttpSessionStateBase session, int id)
+        {
+            if (session["ListaAluno"] != null)
+            {
+                return (session["ListaAluno"] as List<Aluno>).ElementAt(id);
+            }
+            return null;
+        }
+
+        public void Excluir(HttpSessionStateBase session)
+        {
+            if (session["ListaAluno"] != null)
+            {
+                (session["ListaAluno"] as List<Aluno>).Remove(this);
+            }
+        }
+
+        public void Editar(HttpSessionStateBase session, int id)
+        {
+            if (session["ListaAluno"] != null)
+            {
+                var aluno = Aluno.Procurar(session, id);
+                aluno.Nome = this.Nome;
+                aluno.RA = this.RA;
+            }
+        }
+
+        
+    }
+}
---
+++ WebApplication1/Models/Carro.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Carro
+    {
+        public string Placa { get; set; }
+        public int Ano { get; set; }
+        public string Cor { get; set; }
+
+        public static void GerarLista(HttpSessionStateBase session)
+        {
+            if (session["ListaCarro"] != null)
+            {
+                if (((List<Carro>)session["ListaCarro"]).Count > 0)
+                {
+                    return;
+                }
+            }
+
+            var lista = new List<Carro>();
+            lista.Add(new Carro { Placa = "XTZ-3845", Ano = 2018, Cor= "Azul Metálico" });
+            lista.Add(new Carro { Placa = "BRQ-9271", Ano = 2022, Cor = "Preto Fosco" });
+            lista.Add(new Carro { Placa = "LMG-5623", Ano = 2015, Cor = "Vermelho Rubi" });
+
+            session.Remove("ListaCarro");
+            session.Add("ListaCarro", lista);
+        }
+
+        public void Adicionar(HttpSessionStateBase session)
+        {
+            if (session["ListaCarro"] != null)
+            {
+                (session["ListaCarro"] as List<Carro>).Add(this);
+            }
+        }
+
+        public static Carro Procurar(HttpSessionStateBase session, int id)
+        {
+            if (session["ListaCarro"] != null)
+            {
+                return (session["ListaCarro"] as List<Carro>).ElementAt(id);
+            }
+            return null;
+        }
+
+        public void Excluir(HttpSessionStateBase session)
+        {
+            if (session["ListaCarro"] != null)
+            {
+                (session["ListaCarro"] as List<Carro>).Remove(this);
+            }
+        }
+
+        public void Editar(HttpSessionStateBase session, int id)
+        {
+            if (session["ListaCarro"] != null)
+            {
+                var carro = Carro.Procurar(session, id);
+                carro.Placa = this.Placa;
+                carro.Ano = this.Ano;
+                carro.Cor = this.Cor;
+            }
+        }
+    }
+}
